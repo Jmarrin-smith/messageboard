29426712c0856576ee258259519a865610de1444
Jmarrin-smith <marrinsmithj@gmail.com>
2024-07-28 21:55:57 +0100

    project restart
    minimum viable product

--- project with incomplete log in and accounts system/server/seed.js
+++ project with incomplete log in and accounts system/server/seed.js
@@ -5,7 +5,12 @@ dotenv.config();
 
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-db.query(`
+db.query(`create table if not exists users (
+ID serial primary key,
+username varchar(255) ,
+password varchar(255) 
+);
+
 create table if not exists messages(
   id serial primary key,
   username varchar(255) ,
@@ -13,7 +18,8 @@ create table if not exists messages(
   likes int
 );
 
-
+insert into users (username,password) values
+('jamie','adminpassword');
 
 insert into messages (username,message,likes) values
 ('jamie','welcome',0);`);
--- project with incomplete log in and accounts system/server/server.js
+++ project with incomplete log in and accounts system/server/server.js
@@ -8,15 +8,24 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
+const userdb = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+
 const messagedb = new pg.Pool({ connectionString: process.env.MESSAGEDB });
 
 app.get("/", function (request, response) {
   response.json("hello");
 });
 
+app.get("/users", async function (request, response) {
+  // we can use the 'DB' object to query/talk to mypost database.
+  const result = await db.query(`SELECT * FROM users`);
+  const users = result.rows;
+  response.json(users);
+});
+
 app.get("/messages", async function (request, response) {
   // we can use the 'DB' object to query/talk to mypost database.
-  const result = await messagedb.query(`SELECT * FROM messages`);
+  const result = await db.query(`SELECT * FROM messages`);
   const messages = result.rows;
   response.json(messages);
 });
